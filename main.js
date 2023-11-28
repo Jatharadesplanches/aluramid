9deb60e1e6978af855204cd6679fb2f9928bac68
Jatharadesplanches <127160867+Jatharadesplanches@users.noreply.github.com>
2023-11-27 21:28:41 -0300

    alterando o mein

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-function somTocaGrilo (){ 
-     document.querySelector("#som_tecla_grilo").play();
-}
-document.querySelector(".tecla-grilo").onclick = somTocaGrilo;
-
-const listaDeTecla = document.querySelectorAll(".tecla");
-listDeTecla[4].onclick = somTocaGrilo;
+function tocaSom(idElementoAudio){
+     document.querySelector(idElementoAudio).play()
+ }
+ 
+ const listaDeTeclas = document.querySelectorAll (".tecla");
+ 
+ 
+ for(let contador = 0; contador < listaDeTeclas.length; contador++){
+     const tecla = listaDeTeclas[contador];
+     const efeito = tecla.classList[1];
+     const idAudio = `#som_${efeito}`;
+     tecla.onclick = function(){
+       tocaSom(idAudio);
+     }   
+     tecla.onkeydown = function(evento){
+       if(evento.code != 'Tab'){
+       tecla.classList.add('ativa');
+     }
+     tecla.onkeyup = function(){
+       tecla.classList.remove('ativa');
+     }
+ 
+        
+ }
